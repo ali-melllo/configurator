f2eaee58e9c3a7fec20b09ffb9def8f6e3496165
ali-melllo <alimelllo32@gmail.com>
2025-02-17 18:11:24 +0330

    [fix] responsive design

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
 
 import Footer from "@/components/footer";
@@ -10,7 +11,7 @@ import { cn } from "@/lib/utils";
 export default function Page() {
     return (
         <div>
-            <div className="w-full flex p-32 flex-col items-center">
+            <div className="w-full flex pt-28 p-5 md:p-32 flex-col items-center">
                 <GridPattern
                     width={30}
                     height={30}
@@ -21,11 +22,11 @@ export default function Page() {
                         "[mask-image:radial-gradient(1000px_circle_at_center,white,transparent)]",
                     )}
                 />
-                <TextAnimate className="text-6xl text-center font-bold" animation="blurInUp" by="character">
+                <TextAnimate className="text-4xl md:text-6xl text-center font-bold" animation="blurInUp" by="character">
                     Extension
                 </TextAnimate>
                 <p className="text-muted-foreground mt-5">These are our example extensions</p>
-                <section id="photos" className="mt-20">
+                <section id="photos" className="mt-14 md:mt-20">
                     <div className="columns-2 gap-4 sm:columns-3">
                         {GALLERY_DATA.map((imageUrl, idx) => (
                             <BlurFade key={idx + imageUrl.src} delay={0.25 + idx * 0.05} inView>
@@ -42,24 +43,24 @@ export default function Page() {
 
 
 
-            <TextAnimate className="text-6xl text-center font-bold" animation="blurInUp" by="character">
+            <TextAnimate className="text-3xl mt-20 md:text-6xl text-center font-bold" animation="blurInUp" by="character">
                 Projects Overview
             </TextAnimate>
             <p className="text-muted-foreground w-8/12 mx-auto text-center mt-5">Discover our unique renovation and extension projects since 1997. You can also read more about us on Werkspot.</p>
 
             <BlurFade delay={0.25} inView>
-                <div className="flex flex-row-reverse w-9/12 mx-auto h-[22em] my-48">
+                <div className="flex flex-row-reverse w-9/12 mx-auto h-48 md:h-[22em] my-48">
                     <div
                         className="w-6/12 scale-105 z-20">
                         <img
-                            className="w-full rounded-r-3xl h-[22em] rounded-l-lg shadow-2xl"
+                            className="w-full rounded-r-3xl h-48 md:h-[22em] rounded-l-lg shadow-2xl"
                             src={'/extension-project.avif'}
                             alt={'Rating'}
                         />
                     </div>
-                    <div className="w-6/12 h-[22em] p-10 border rounded-r-sm rounded-l-3xl bg-background">
-                        <h2 className="text-5xl font-bold">Extension Project</h2>
-                        <p className="mt-10 text-muted-foreground text-lg">
+                    <div className="w-6/12 h-48 md:h-[22em] p-3 md:p-10 border rounded-r-sm rounded-l-3xl bg-background">
+                        <h2 className="md:text-5xl font-bold">Extension Project</h2>
+                        <p className="mt-3 md:mt-10 text-muted-foreground text-xs md:text-lg">
                             Discover how we can enhance your home with craftsmanship and attention to detail, so that you really feel at home.
                         </p>
                     </div>
@@ -67,30 +68,29 @@ export default function Page() {
             </BlurFade>
 
             <BlurFade delay={0.25} inView>
-                <div className="flex w-9/12 mx-auto h-[22em] my-48">
+                <div className="flex w-9/12 mx-auto h-48 md:h-[22em] my-48">
                     <div
                         className="w-6/12 scale-105 z-20">
                         <img
-                            className="w-full rounded-l-3xl h-[22em] rounded-r-lg "
+                            className="w-full rounded-l-3xl h-48 md:h-[22em] rounded-r-lg "
                             src={'/renovation-project.avif'}
                             alt={'Rating'}
                         />
                     </div>
-                    <div className="w-6/12 h-[22em] p-10 border rounded-l-sm rounded-r-3xl bg-background">
-                        <h2 className="text-5xl font-bold">Renovation Project</h2>
-                        <p className="mt-10 text-muted-foreground text-lg">
+                    <div className="w-6/12 h-48 md:h-[22em] p-2 pl-3 md:p-10 border rounded-l-sm rounded-r-3xl bg-background">
+                        <h2 className="md:text-5xl font-bold">Renovation Project</h2>
+                        <p className="mt-2 md:mt-10 text-muted-foreground text-xs md:text-lg">
                             View our recent renovation projects that combine elegance and tradition for a perfect home experience.
                         </p>
                     </div>
                 </div>
             </BlurFade>
 
-            <div className="w-full flex p-32 my-20 flex-col items-center">
-
-                <TextAnimate className="text-6xl text-center font-bold" animation="blurInUp" by="character">
+            <div className="w-full flex p-5 md:p-32 my-20 flex-col items-center">
+                <TextAnimate className="text-4xl md:text-6xl text-center font-bold" animation="blurInUp" by="character">
                     Projects
                 </TextAnimate>
-                <p className="text-muted-foreground mt-5">Discover our renovations and extensions for your dream home</p>
+                <p className="text-muted-foreground text-center mt-5">Discover our renovations and extensions for your dream home</p>
                 <section id="photos" className="mt-20">
                     <div className="columns-2 gap-4 sm:columns-3">
                         {PROJECTS_GALLERY_DATA.map((imageUrl, idx) => (
@@ -108,16 +108,16 @@ export default function Page() {
 
 
             <BlurFade delay={0.25} inView>
-                <TextAnimate className="text-6xl text-center font-bold" animation="blurInUp" by="character">
+                <TextAnimate className="text-2xl -mt-10 md:text-6xl text-center font-bold" animation="blurInUp" by="character">
                     Customer reviews of Werkspot
                 </TextAnimate>
-                <p className="text-muted-foreground w-6/12 mx-auto text-center mt-5">Read here the experiences of our satisfied customers about our projects</p>
+                <p className="text-muted-foreground md:w-6/12 mx-auto text-center mt-5">Read here the experiences of our satisfied customers about our projects</p>
 
-                <div className="flex w-9/12 mx-auto h-[25em] my-20 mb-48">
-                    <div className="w-6/12 h-full p-10  shadow-2xl border rounded-r-sm rounded-l-3xl bg-background">
-                        <h2 className="text-2xl font-bold text-primary">★★★★★</h2>
-                        <h2 className="text-lg mt-2">Home Renovation: Kitchen, Living Room/Dining Room, Bedroom(s)</h2>
-                        <p className="mt-5 text-muted-foreground w-10/12">
+                <div className="flex md:w-9/12 mx-auto pl-3 md:h-[25em] my-20 mb-48">
+                    <div className="w-6/12 h-full p-5 md:p-10  shadow-2xl border rounded-r-sm rounded-l-3xl bg-background">
+                        <h2 className="md:text-2xl font-bold text-primary">★★★★★</h2>
+                        <h2 className="md:text-lg mt-2">Home Renovation: Kitchen, Living Room/Dining Room, Bedroom(s)</h2>
+                        <p className="mt-2 md:mt-5 text-xs md:text-base text-muted-foreground w-10/12">
                             {"Persian Top helped me tremendously with the construction and painting of my new apartment. Not only did they do a very good job, Abbas also gave me very good advice regarding the best solutions for certain problems in the apartment. I am very happy with the result!"}
                         </p>
                         <div className="flex items-center mt-5 gap-5">
@@ -126,13 +126,13 @@ export default function Page() {
                                 src={'/farmer.avif'}
                                 alt={'Rating'}
                             />
-                            <p className="text-lg text-muted-foreground ">The Farmer</p>
+                            <p className="md:text-lg text-muted-foreground ">The Farmer</p>
                         </div>
                     </div>
-                    <div className="w-6/12 z-20 h-full p-10 -ml-5 shadow-2xl scale-110 border rounded-l-sm rounded-r-3xl bg-background">
-                        <h2 className="text-2xl font-bold text-primary">★★★★★</h2>
-                        <h2 className="text-lg mt-2">Add-on: 30 m²; Yes; Flat roof</h2>
-                        <p className="mt-5 text-muted-foreground w-10/12">
+                    <div className="w-6/12 z-20 h-full p-3 md:p-10 -ml-5 shadow-2xl scale-110 border rounded-l-sm rounded-r-3xl bg-background">
+                        <h2 className="md:text-2xl font-bold text-primary">★★★★★</h2>
+                        <h2 className="md:text-lg mt-2">Add-on: 30 m²; Yes; Flat roof</h2>
+                        <p className="mt-3 text-xs md:text-base md:mt-5 text-muted-foreground w-10/12">
                             {"Persian Top helped me tremendously with the construction and painting of my new apartment. Not only did they do a very good job, Abbas also gave me very good advice regarding the best solutions for certain problems in the apartment. I am very happy with the result!"}
                         </p>
                         <div className="flex items-center gap-5 mt-5">
@@ -141,20 +141,16 @@ export default function Page() {
                                 src={'/farhad.avif'}
                                 alt={'Rating'}
                             />
-                            <p className="text-lg text-muted-foreground ">Fahrhad Sarabi</p>
+                            <p className="md:text-lg text-muted-foreground ">Fahrhad Sarabi</p>
                         </div>
-                    </div>
-
-                    
-
-                    
+                    </div>                    
                 </div>
 
-                <div className="flex flex-row-reverse w-9/12 mx-auto  my-20 mb-48">
-                    <div className="w-6/12 h-full p-10 border rounded-l-sm rounded-r-3xl shadow-2xl pl-20 bg-background">
-                        <h2 className="text-2xl font-bold text-primary">★★★★★</h2>
-                        <h2 className="text-lg mt-2">Complete renovation including extension and new dormer window</h2>
-                        <p className="mt-5 text-muted-foreground w-10/12">
+                <div className="flex flex-row-reverse pl-3 md:w-9/12 mx-auto mt-48 my-20 mb-48">
+                    <div className="w-6/12 h-full p-3 md:p-10 border rounded-l-sm rounded-r-3xl shadow-2xl pl-5 md:pl-20 bg-background">
+                        <h2 className="md:text-2xl font-bold text-primary">★★★★★</h2>
+                        <h2 className="md:text-lg mt-2">Complete renovation including extension and new dormer window</h2>
+                        <p className="mt-5 text-xs md:text-base text-muted-foreground w-10/12">
                             {"Abbas and his men made our renovation a great success. We simply have a new house! The renovation included an extension where the foundation had to be adjusted. This was done in the most chic way to prevent a lowering of the ceiling. This despite the fact that this cost Abbas much more time and money while it was included in the contracted work. These men know what they are doing, communicate a lot, in all 4 months that they were working always reachable, always open to consultation, always reasonable. It is really very pleasant to work with these men. Real professionals who know what they are talking about and what they are doing. In short, I heartily recommend Persian Top to anyone who has serious extensive and possibly complex renovation wishes."}
                         </p>
                         <div className="flex items-center mt-5 gap-5">
@@ -163,13 +159,13 @@ export default function Page() {
                                 src={'/remmert.avif'}
                                 alt={'Rating'}
                             />
-                            <p className="text-lg text-muted-foreground ">Remmert family</p>
+                            <p className="text-xs md:text-lg text-muted-foreground ">Remmert family</p>
                         </div>
                     </div>
-                    <div className="w-6/12 z-20 h-full p-10 border scale-110 shadow-2xl rounded-r-sm rounded-l-3xl bg-background">
-                        <h2 className="text-2xl font-bold text-primary">★★★★★</h2>
-                        <h2 className="text-lg mt-2">Renovation of kitchen and hallway</h2>
-                        <p className="mt-5 text-muted-foreground w-10/12">
+                    <div className="w-6/12 z-20 h-full p-3 md:p-10 border scale-110 shadow-2xl rounded-r-sm rounded-l-3xl bg-background">
+                        <h2 className="md:text-2xl font-bold text-primary">★★★★★</h2>
+                        <h2 className="md:text-lg mt-2">Renovation of kitchen and hallway</h2>
+                        <p className="mt-5 text-xs md:text-base text-muted-foreground w-10/12">
                             {"Persian Top has carried out the assignment fantastically. The work delivered is of high quality. Persian Top started on the agreed day and worked continuously on the project with 2 or 3 men. The work took longer than planned, but the demands were high and there were a number of unforeseen things. This resulted in a budget item, but this was a small percentage of the total final amount. With a house from the 1930s, you unfortunately cannot foresee everything and you have to calculate a budget item. In that respect, Persian Top has handled things properly. I had contact with Sander, but in the work with Abbas. The communication with Abbas was perfect, decisions were always made in good consultation and he carried out the work as I wanted, he also gave good advice and was well versed in all aspects of the contractor's profession. The work consisted of the following components: demolition (no special features). Plastering of the hall, corridor and kitchen with a wainscoting, whereby the lower part of the plasterwork was applied slightly thicker. The plasterwork was delivered very smooth and tight. The kitchen is prepared (electricity, gas and water). Finally, the new tiled floor in the hall, hallway and kitchen. Quite a challenge. It involved Winckelman tiles of 10x10 cm in mosaic (three colours) with a small joint of 1.5 mm. This is quite a difficult job if you consider that the tiles were not completely equal and the joint extremely small. But the end result is worth it"}
                         </p>
                         <div className="flex items-center gap-5 mt-5">
@@ -178,7 +174,7 @@ export default function Page() {
                                 src={'/jan.avif'}
                                 alt={'Rating'}
                             />
-                            <p className="text-lg text-muted-foreground ">Jan Floor</p>
+                            <p className="text-sm md:text-lg text-muted-foreground ">Jan Floor</p>
                         </div>
                     </div>
 
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
 
 import Footer from "@/components/footer";
@@ -21,47 +22,46 @@ export default function Page() {
                     "[mask-image:radial-gradient(1000px_circle_at_center,white,transparent)]",
                 )}
             />
-            <TextAnimate className="text-6xl text-center font-bold" animation="blurInUp" by="character">
+            <TextAnimate className="text-3xl md:text-6xl text-center font-bold" animation="blurInUp" by="character">
                 Our Services
             </TextAnimate>
-            <p className="text-muted-foreground mt-5">Quality and craftsmanship for renovation and expansion.</p>
+            <p className="text-muted-foreground text-center mt-5">Quality and craftsmanship for renovation and expansion.</p>
 
-            <div className="w-10/12 z-20 flex gap-5 mt-10">
-
-                <div className="w-4/12 p-8 bg-background dark:bg-black shadow-xl rounded-2xl flex flex-col">
+            <div className="md:w-10/12 items-center flex flex-col md:flex-row z-20 gap-5 mt-10">
+                <div className="w-11/12 md:w-4/12 p-8 bg-background dark:bg-black shadow-xl rounded-2xl flex flex-col">
                     <h2 className="font-extrabold text-3xl">Complete Renovation</h2>
                     <p className="text-muted-foreground mt-5"> Transform your home with our expertise in complete renovations and high-quality finishes for every project.</p>
                     <img
                         className="mt-5 min-h-96 rounded-xl w-full"
-                        src={'/renovation.avif'}
+                        src={'/renovation-project.avif'}
                         alt={'Renovation'}
                     />
                 </div>
-                <div className="w-4/12 p-8  bg-background dark:bg-black shadow-xl rounded-2xl flex flex-col">
+                <div className="w-11/12 md:w-4/12 p-8  bg-background dark:bg-black shadow-xl rounded-2xl flex flex-col">
                     <h2 className="font-extrabold text-3xl">Home extensions</h2>
                     <p className="text-muted-foreground mt-5">Create extra space with elegant home extensions that perfectly match your living needs and style</p>
                     <img
                         className="mt-5 min-h-96 rounded-xl w-full"
-                        src={'/extension.avif'}
+                        src={'/extension-project.avif'}
                         alt={'Extension'}
                     />
                 </div>
-                <div className="w-4/12 p-8 bg-background dark:bg-black shadow-xl rounded-2xl flex flex-col">
+                <div className="w-11/12 md:w-4/12 p-8 bg-background dark:bg-black shadow-xl rounded-2xl flex flex-col">
                     <h2 className="font-extrabold text-3xl">Craftsmanship</h2>
                     <p className="text-muted-foreground mt-5">Experience the highest quality and craftsmanship in all our renovation and expansion projects.</p>
                     <img
                         className="mt-5 min-h-96 rounded-xl w-full"
-                        src={'/craft.avif'}
+                        src={'/services-main.avif'}
                         alt={'Craft'}
                     />
                 </div>
             </div>
 
-            <div
-                className="bg-center my-48 bg-cover bg-no-repeat bg-body-img bg-fixed container flex items-center rounded-lg justify-center h-[35em]">
-                <div
-                    className="flex w-full justify-center items-center gap-5">
-                    <div className="w-3/12 z-20 h-full p-10 shadow-2xl rounded-2xl border bg-accent/50">
+            <div className="bg-center w-full my-48 bg-cover bg-no-repeat bg-body-img md:bg-fixed bg-local container flex items-center rounded-lg justify-center md:h-[35em]">
+
+               <div
+                    className="flex flex-col md:flex-row w-full justify-center py-5 items-center gap-5">
+                    <div className="md:w-3/12 z-20 h-full p-10 shadow-2xl rounded-2xl border bg-accent/50">
                         <h2 className="text-3xl font-bold text-primary">★★★★★</h2>
                         <p className="mt-5 text-lg">
                             {" we are very satisfied with the work of Persian top and would definitely recommend him for a job."}
@@ -77,7 +77,7 @@ export default function Page() {
                             <p className="text-lg">Peter G</p>
                         </div>
                     </div>
-                    <div className="w-3/12 z-20 h-full p-10 shadow-2xl rounded-2xl border bg-accent/50">
+                    <div className="md:w-3/12 z-20 h-full p-10 shadow-2xl rounded-2xl border bg-accent/50">
                         <h2 className="text-3xl font-bold text-primary">★★★★★</h2>
                         <p className="mt-5 text-lg">
                             {"Great experience with renovation, craftsmanship and attention to detail. My house now feels like new!"}
@@ -93,7 +93,7 @@ export default function Page() {
                             <p className="text-lg">Sophie K</p>
                         </div>
                     </div>
-                    <div className="w-3/12 z-20 h-full p-10 shadow-2xl rounded-2xl border bg-accent/50">
+                    <div className="md:w-3/12 z-20 h-full p-10 shadow-2xl rounded-2xl border bg-accent/50">
                         <h2 className="text-3xl font-bold text-primary">★★★★★</h2>
                         <p className="mt-5 text-lg">
                             {"Personal approach and great results. Our expansion is perfect and exceeds all our expectations!"}
@@ -114,25 +114,25 @@ export default function Page() {
 
 
             <BlurFade delay={0.25} inView>
-                <div className="flex flex-row-reverse w-9/12 mx-auto h-[22em] mb-48">
+                <div className="flex flex-col items-center md:flex-row-reverse w-11/12 md:w-9/12 mx-auto md:h-[22em] mb-48">
                     <Link
                         href={'https://www.google.com/maps/place/Dreef+10,+1431+WD+Aalsmeer,+Netherlands/@52.2555208,4.7631746,15.25z/data=!4m15!1m8!3m7!1s0x47c5de1983ba779b:0x653cdd2b018b0dcc!2sDreef+10,+1431+WD+Aalsmeer,+Netherlands!3b1!8m2!3d52.2572332!4d4.763412!16s%2Fg%2F11c18h8xnz!3m5!1s0x47c5de1983ba779b:0x653cdd2b018b0dcc!8m2!3d52.2572332!4d4.763412!16s%2Fg%2F11c18h8xnz!5m2!1e4!1e2?hl=en-US&entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D'}
-                        className="w-6/12 scale-105 z-20 opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-200">
+                        className="md:w-6/12 scale-105 z-20 opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-200">
                         <img
-                            className="w-full rounded-r-3xl h-[22em] rounded-l-lg shadow-2xl"
+                            className="w-full rounded-lg md:rounded-r-3xl h-[22em] rounded-l-lg shadow-2xl"
                             src={'/location.png'}
                             alt={'Rating'}
                         />
                     </Link>
-                    <div className="w-6/12 h-[22em] p-10 border rounded-r-sm rounded-l-3xl bg-background">
-                        <h2 className="text-5xl font-bold">Locations</h2>
-                        <p className="mt-5 text-muted-foreground text-sm">
+                    <div className="md:w-6/12 md:h-[22em] p-5 md:p-10 border rounded-r-sm rounded-lg md:rounded-l-3xl mt-5 md:mt-0 bg-background">
+                        <h2 className="text-xl md:text-5xl font-bold">Locations</h2>
+                        <p className="mt-5 text-muted-foreground text-xs md:text-sm">
                             We offer our services in various regions. Please contact us for more information about your specific location
                         </p>
                         <div className="flex flex-col items-start mt-5">
-                            <h2 className="font-bold text-lg">Address</h2>
+                            <h2 className="font-bold md:text-lg">Address</h2>
                             <p className="text-muted-foreground">Dreef 10, 1431WD Aalsmeer</p>
-                            <h2 className="font-bold text-lg mt-5">Opening hours </h2>
+                            <h2 className="font-bold md:text-lg mt-5">Opening hours </h2>
                             <p className="text-muted-foreground">Mon-Fri 9am-5pm</p>
                         </div>
                     </div>
