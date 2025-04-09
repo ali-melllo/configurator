aa14586ce3eca47e1b018e8adcc78da2809f710f
ali-melllo <alimelllo32@gmail.com>
2025-04-09 13:55:20 +0330

    adding dutch

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -50,12 +50,12 @@ export default function Page() {
           )}
         />
 
-        <div className="md:pl-36 md:pr-24 flex w-full mt- md:mt-0 md:w-6/12 flex-col justify-center">
+        <div className="md:pl-36 md:pr-24 flex w-full md:mt-0 md:w-6/12 flex-col justify-center">
           <TextAnimate className="text-3xl break-words text-wrap mx-auto text-center md:text-left md:text-5xl font-bold" duration={500} animation="blurInUp" by="word">
             {t('title')}
           </TextAnimate>
           <p className="text-muted-foreground text-sm md:text-base text-center md:text-left mt-5">
-            Experience craftsmanship and quality since 1998 and read our reviews at:
+            {t('description')}
             <a
               href="https://www.werkspot.nl/profiel/persian-top/reviews"
               className="text-blue-600 ml-1 hover:underline"
@@ -68,16 +68,16 @@ export default function Page() {
 
           <Button className="h-12 lg:h-14 z-30 px-5 mt-5 mx-auto md:mt-10 w-11/12 md:w-full font-semibold text-base md:text-2xl rounded-xl shadow-2xl">
             <Link href={'/configurator'}>
-              Request a Quote Using 3D Configurator
+              {t("3dButton")}
             </Link>
           </Button>
           <Button
             onClick={() => dispatch(setBuildingStep(0))}
             className="h-12 lg:h-14 z-30 px-5 mt-5 w-11/12 md:w-full mx-auto  font-semibold text-base md:text-2xl rounded-xl shadow-2xl">
-            Request a Quote Manually
+            {t('manualButton')}
           </Button>
           <Button className="h-12 lg:h-14 z-30 px-5 mt-5 w-11/12 md:w-full mx-auto  font-semibold text-base md:text-2xl rounded-xl shadow-2xl">
-            Request a Quote direct phone Call
+            {t('phoneButton')}
           </Button>
         </div>
 
@@ -105,7 +105,7 @@ export default function Page() {
 
       {buildingStep === null && <div className="mt-48 w-full flex flex-col items-center">
         <TextAnimate className="text-3xl md:text-6xl text-center font-bold" animation="blurInUp" by="character">
-          How Does it Work ?
+          {t('roadMapTitle')}
         </TextAnimate>
 
         <div className="w-8/12 flex flex-col md:flex-row gap-y-16 md:gap-y-10 gap-10 mt-36">
@@ -113,8 +113,8 @@ export default function Page() {
             <div className="bg-primary absolute size-20 flex justify-center items-center -mt-10 rounded-3xl shadow-xl">
               <LayoutDashboard className="size-14" color="#ffffff" />
             </div>
-            <h2 className="mt-16 font-medium text-xl"><span className="font-extrabold text-4xl mt-1 mr-1">1#</span> Choose Your Preferred Services in Configurator Easy From Webapp</h2>
-            <p className="text-muted-foreground mt-5">Persian top offers an smooth and easy to use configurator for selecting specific home exterior and inside house services</p>
+            <h2 className="mt-16 font-medium text-xl"><span className="font-extrabold text-4xl mt-1 mr-1">1#</span>{t('roadMapOneTitle')}</h2>
+            <p className="text-muted-foreground mt-5">{t('roadMapOneDescription')}</p>
             <Button className="rounded-xl w-full h-12 mt-5 font-bold text-lg">
               Go To Configurator
             </Button>
@@ -123,21 +123,21 @@ export default function Page() {
             <div className="bg-primary absolute size-20 flex justify-center items-center -mt-10 rounded-3xl shadow-xl">
               <Phone className="size-14" color="#ffffff" />
             </div>
-            <h2 className="mt-16 font-medium text-xl"><span className="font-extrabold text-4xl mt-1 mr-1">2#</span> Professional craftsmen will contact you to finalize your requests and prices </h2>
-            <p className="text-muted-foreground mt-5">Inside the configurator You'll see the estimated price real time and you can request a quote to get the exact price and finalize your order.</p>
+            <h2 className="mt-16 font-medium text-xl"><span className="font-extrabold text-4xl mt-1 mr-1">2#</span> {t('roadMapTwoTitle')} </h2>
+            <p className="text-muted-foreground mt-5">{t('roadMapTwoDescription')}</p>
 
           </div>
           <div className="w-full md:w-4/12 p-5 bg-background dark:bg-accent/30 shadow-xl rounded-2xl flex flex-col items-center relative">
             <div className="bg-primary absolute size-20 flex justify-center items-center -mt-10 rounded-3xl shadow-xl">
               <Wrench className="size-14" color="#ffffff" />
             </div>
-            <h2 className="mt-16 font-medium text-xl"><span className="font-extrabold text-4xl mt-1 mr-1">3#</span> Let the experts bring your needs in real life in a very short time </h2>
-            <p className="text-muted-foreground mt-5">Your specialist in renovations and home extensions with more than 25 years of experience and craftsmanship will deliver your needs</p>
+            <h2 className="mt-16 font-medium text-xl"><span className="font-extrabold text-4xl mt-1 mr-1">3#</span>{t('roadMapThreeTitle')} </h2>
+            <p className="text-muted-foreground mt-5">{t('roadMapThreeDescription')}</p>
           </div>
         </div>
       </div>}
 
-      {buildingStep === null && <div className="mt-48 w-full flex flex-col items-center">
+      {/* {buildingStep === null && <div className="mt-48 w-full flex flex-col items-center">
 
         <TextAnimate className="text-3xl w-full lg:w-6/12 md:text-6xl text-center font-bold" animation="blurInUp" by="word">
           Super fast solution for your House extensions and services
@@ -150,43 +150,45 @@ export default function Page() {
             alt={'Persian Top'}
           />
         </div>
-      </div>}
+      </div>} */}
 
       {buildingStep === null && <div className="my-48 w-full flex flex-col items-center">
         <TextAnimate className="text-3xl md:text-6xl text-center font-bold" animation="blurInUp" by="character">
-          Our Services
+          {t('services.servicesTitle')}
         </TextAnimate>
-        <p className="text-muted-foreground text-center mt-5">Quality and craftsmanship for renovation and expansion.</p>
+        <p className="text-muted-foreground text-center mt-5">{t('services.servicesDescription')}</p>
 
         <div className="w-11/12 md:w-10/12 flex flex-col md:flex-row gap-y-20 md:gap-y-10 gap-10 mt-36">
 
           <div className="md:w-4/12 p-8 bg-background dark:bg-accent/30 shadow-xl rounded-2xl flex flex-col">
-            <h2 className="font-extrabold text-3xl">Complete Renovation</h2>
-            <p className="text-muted-foreground mt-5"> Transform your home with our expertise</p>
+            <h2 className="font-extrabold text-3xl">{t('services.completeRenovation.title')}</h2>
+            <p className="text-muted-foreground mt-5">{t('services.completeRenovation.description')}</p>
             <img
               className="mt-5 min-h-96 rounded-2xl w-full"
-              src={'/renovation-project.avif'}
-              alt={'Renovation'}
+              src="/renovation-project.avif"
+              alt="Renovation"
             />
           </div>
           <div className="md:w-4/12 p-8 scale-110 bg-background dark:bg-accent/30 shadow-xl rounded-2xl flex flex-col">
-            <h2 className="font-extrabold text-3xl">Home extensions</h2>
-            <p className="text-muted-foreground mt-5">Beautiful extensions for your living needs.</p>
+            <h2 className="font-extrabold text-3xl">{t('services.homeExtensions.title')}</h2>
+            <p className="text-muted-foreground mt-5">{t('services.homeExtensions.description')}</p>
             <img
               className="mt-5 min-h-96 rounded-2xl w-full"
-              src={'/extension-project.avif'}
-              alt={'Extension'}
+              src="/extension-project.avif"
+              alt="Extension"
             />
           </div>
+
           <div className="md:w-4/12 p-8 bg-background dark:bg-accent/30 shadow-xl rounded-2xl flex flex-col">
-            <h2 className="font-extrabold text-3xl">Craftsmanship</h2>
-            <p className="text-muted-foreground mt-5">Elegance and tradition in every project.</p>
+            <h2 className="font-extrabold text-3xl">{t('services.craftsmanship.title')}</h2>
+            <p className="text-muted-foreground mt-5">{t('services.craftsmanship.description')}</p>
             <img
               className="mt-5 min-h-96 rounded-2xl w-full"
-              src={'/services-main.avif'}
-              alt={'Craft'}
+              src="/services-main.avif"
+              alt="Extension"
             />
           </div>
+
         </div>
       </div>}
 
@@ -194,9 +196,11 @@ export default function Page() {
 
       {buildingStep === null && <div className="w-11/12 md:w-9/12 mx-auto my-48">
         <TextAnimate className="text-3xl md:text-6xl text-center font-bold" animation="blurInUp" by="character">
-          Gallery
+          {t('gallery.title')}
         </TextAnimate>
-        <p className="text-muted-foreground text-center mt-5">Discover our renovations and extensions with craftsmanship and detail.</p>
+        <p className="text-muted-foreground text-center mt-5">
+          {t('gallery.description')}
+        </p>
 
         <section id="photos" className="mt-20">
           <div className="columns-2 gap-4 sm:columns-3">
@@ -223,10 +227,10 @@ export default function Page() {
               alt={'Rating'}
             />
           </div>
-          <div className="w-full md:w-6/12 z-20 h-full p-5 md:p-10 md:-ml-5 shadow-2xl scale-110 border md:rounded-l-sm rounded-lg md:rounded-r-3xl bg-background">
+          <div className="w-full md:w-6/12 z-20 flex flex-col justify-between h-full p-5 md:p-10 md:-ml-5 shadow-2xl scale-110 border md:rounded-l-sm rounded-lg md:rounded-r-3xl bg-background">
             <h2 className="md:text-4xl font-bold text-primary">★★★★★</h2>
             <p className="mt-5 md:text-lg">
-              {"Abbas keeps his promises and the coordination during the renovation went smoothly. In short, we are very satisfied with the work of Persian top and would definitely recommend him for a job."}
+              {t('feedback')}
             </p>
             <div className="flex flex-col items-start mt-5">
               <img
